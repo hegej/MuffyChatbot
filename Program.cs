bf2b124970abc332514af5375fcbcbfb15e60acd
hegej <hege.jacobsen12@gmail.com>
2023-04-26 13:04:46 +0200

    Edit program.cs client code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSharpPlus;
+
+var client = new DiscordClient(new DiscordConfiguration {
+    Token = "MY TOKEN",
+    TokenType = TokenType.Bot
+});
+
+client.MessageCreated += async (client, args) => {
+
+    if (args.Message.Content.StartsWith("ping")) {
+        await client.SendMessageAsync(args.Channel, "pong");
+    }
+        
+};
+
+await client.ConnectAsync();
+await Task.Delay(-1);
+
+
+
